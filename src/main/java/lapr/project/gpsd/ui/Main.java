57837f548c317c038f1a741581d715f321699dd7
Carlos Manuel Abreu Gomes Ferreira <cgf@isep.ipp.pt>
2019-05-25 16:26:26 +0100

    IT1 ESOFT

--- src/main/java/lapr/project/gpsd/ui/Main.java
+++ src/main/java/lapr/project/gpsd/ui/Main.java
@@ -17,7 +17,7 @@ public class Main
     public static void main(String[] args)
     {
         try
-        {
+        {System.out.println("Hello World");
             MenuUI uiMenu = new MenuUI();
 
             uiMenu.run();
