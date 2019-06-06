bf6eeef411bbac21f1aceb7fac29cb57f71fed81
Antonio Freitas <1141729@isep.ipp.pt>
2019-06-06 02:44:40 +0100

    Add class ServiceProvider, ServiceProviderRegistry, SPCategory, SPGeographicAreaList [UC-08] [Implementation]

---
+++ src/main/java/lapr/project/gpsd/model/ServiceProviderRegistry.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.gpsd.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author astfr
+ */
+public class ServiceProviderRegistry {
+    
+    private List<ServiceProvider> spList;
+    
+}
