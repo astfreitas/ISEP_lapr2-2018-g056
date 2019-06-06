351465d3202d3cf1d34ac2cbd7b88298d8663e23
Antonio Freitas <1141729@isep.ipp.pt>
2019-06-06 02:45:22 +0100

    Add class GeographicAreaRegistry [UC-05] [Implementation]

---
+++ src/main/java/lapr/project/gpsd/model/GeographicAreaRegistry.java
@@ -0,0 +1,18 @@
+package lapr.project.gpsd.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author astfr
+ */
+public class GeographicAreaRegistry {
+    
+    /**
+     * List of Geographic Areas known by the company
+     */
+    private List<GeographicArea> geoAreaList = new ArrayList<>();
+    
+    
+}
