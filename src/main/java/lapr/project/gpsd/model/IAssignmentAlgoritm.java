1c88465ca2d0387c1b144e649e87687a911b3c67
Breno Pacheco <1180005@isep.ipp.pt>
2019-06-06 19:52:14 +0100

    Fixes and javadocs addition [UC-10][Javadocs and impl.]

--- src/main/java/lapr/project/gpsd/model/IAssignmentAlgoritm.java
+++ src/main/java/lapr/project/gpsd/model/IAssignmentAlgoritm.java
@@ -1,6 +1,22 @@
 package lapr.project.gpsd.model;
 
+import java.util.List;
+
 public interface IAssignmentAlgoritm {
 
-    
+    /**
+     * Takes the List of ServiceDescriptions not yet assigned (assigned
+     * attribute false) and assign each one to an available Service Provider
+     * according to the following criteria:
+     *
+     * 1.2. 3.
+
+ The AssignmentAlgoritm implementation differs in the scheduling of
+ ServiceDescriptions (FIFO, random, etc)
+     *
+     * @param sqr
+     * @param spr
+     * @return List of new Service Assignments to be added to Registry
+     */
+    public List<ServiceAssignment> assignServices(ServiceRequestRegistry sqr, ServiceProviderRegistry spr);
 }
