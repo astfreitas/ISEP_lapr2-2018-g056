6e056e5d2981d44424464b9d6b303fe34944f908
Breno Pacheco <1180005@isep.ipp.pt>
2019-06-07 23:33:54 +0100

    Login UI running [UI]

---
+++ src/main/java/lapr/project/gpsd/ui/Main.java
@@ -0,0 +1,32 @@
+package lapr.project.gpsd.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginScreen.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Very awesome application :O");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
