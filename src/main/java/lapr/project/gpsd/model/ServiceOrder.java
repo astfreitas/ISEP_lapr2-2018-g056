a6f43e3c444f730d8e2ea1d2cb0ed64c11a86d95
M João Dias <1171865@isep.ipp.pt>
2019-06-07 19:52:58 +0100

    Add class ServiceOrder [UC-13] [Implementation]

--- src/main/java/lapr/project/gpsd/model/ServiceOrder.java
+++ src/main/java/lapr/project/gpsd/model/ServiceOrder.java
@@ -1,4 +1,62 @@
 package lapr.project.gpsd.model;
 
-class ServiceOrder {
+import static lapr.project.gpsd.model.Constants.*;
+
+public class ServiceOrder {
+
+    private ServiceRequestDescription servDesc;
+    private ServiceProvider servPorvider;
+    private ServiceRequest serRequest;
+    private SchedulePreference schedPreference;
+    private String id;
+    private String status;
+
+    public ServiceOrder(ServiceRequestDescription servDesc, ServiceProvider servPorvider, ServiceRequest serRequest, SchedulePreference schedPreference, String id, String status) {
+        this.servDesc = servDesc;
+        this.servPorvider = servPorvider;
+        this.serRequest = serRequest;
+        this.schedPreference = schedPreference;
+        this.id = id;
+        this.status = status;
+    }
+
+    public ServiceRequestDescription getServDesc() {
+        return servDesc;
+    }
+
+    public ServiceProvider getServPorvider() {
+        return servPorvider;
+    }
+
+    public ServiceRequest getSerRequest() {
+        return serRequest;
+    }
+
+    public SchedulePreference getSchedPreference() {
+        return schedPreference;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public boolean isPending(String status) {
+        return this.status == PENDING_ORDER;
+    }
+    
+    /**
+     * Checks if the Service Order has a determined ID
+     * 
+     * @param id ID to check
+     * @return true or false
+     */
+    public boolean hasId(String id)
+    {
+        return this.id.equalsIgnoreCase(id);
+    }
+
 }
