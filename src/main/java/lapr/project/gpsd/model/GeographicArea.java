77d0a57c9900113b1acc53de17bfb592b1004389
M João Dias <1171865@isep.ipp.pt>
2019-06-09 18:50:08 +0100

    add attribute geoId, respective getter and method hasId [UC-08] [Implementation]

--- src/main/java/lapr/project/gpsd/model/GeographicArea.java
+++ src/main/java/lapr/project/gpsd/model/GeographicArea.java
@@ -14,6 +14,7 @@ import lapr.project.gpsd.controller.ApplicationGPSD;
  */
 public class GeographicArea {
 
+    private String geoId;
     /**
      * Geographic area designation
      */
@@ -38,35 +39,56 @@ public class GeographicArea {
 
     /**
      * Geographic Area constructor with some atributos received by as method
-     * parameters and the PostalCode objeto created by the string received and
+     * parameters and the PostalCode object created by the string received and
      * List of Locations by external Service. Throws Excepetion from PostalCode
      * Constructor.
      *
+     * @param geoId if of the Geographic Area
      * @param designation Designation string for the Geographic Area
      * @param cost Travel Cost to this Geographic Area
      * @param radius Radius double for the area of action of the GeoArea
      * @param mainPostalCode Postal Code string for the center of the GeoArea
      * @param exService reference for the External Service to use to obtain the
      * list of locations for this Geo Area
-     * @param pcReg PostalCodeRegistry needed to obtain the existing PostalCode List in system
+     * @param pcReg PostalCodeRegistry needed to obtain the existing PostalCode
+     * List in system
      * @throws IllegalArgumentException from PostalCode Constructor and from the
      * this constructor.
      */
-    public GeographicArea(String designation, double cost, double radius,
-            String mainPostalCode, IExternalService exService, PostalCodeRegistry pcReg) {
-        if ((designation == null) || (mainPostalCode == null)
-                || (exService == null)) {
+    public GeographicArea(String geoId, String designation, double cost, double radius, String mainPostalCode, IExternalService exService, PostalCodeRegistry pcReg) {
+        if ((geoId == null) || (designation == null) || (mainPostalCode == null)
+                || (exService == null) || (geoId.isEmpty())) {
             throw new IllegalArgumentException("None of the arguments can be null or empty.");
         } else if (cost <= 0 || radius <= 0) {
-                throw new IllegalArgumentException("Travel Cost or Radius cannot"
-                        + "have zero or negative values");
+            throw new IllegalArgumentException("Travel Cost or Radius cannot"
+                    + "have zero or negative values");
         }
+        this.geoId = geoId;
         this.designation = designation;
         this.travelCost = cost;
         this.radius = radius;
         this.mainPostalCode = ApplicationGPSD.getInstance().getCompany().getPostalCodeRegistry().getMatchingPostalCode(mainPostalCode);
         this.LocationList = exService.getActsOnLocationList(this.mainPostalCode, radius, pcReg);
-        
+
+    }
+
+    /**
+     * Gets the Geographic area unique ID
+     *
+     * @return Geographic area unique ID
+     */
+    public String getGeoId() {
+        return geoId;
+    }
+
+    /**
+     * Checks if the Geographic area has a determined ID
+     *
+     * @param id ID to check
+     * @return True/false if the Geographic area has/hasn't the ID
+     */
+    public boolean hasId(String id) {
+        return this.geoId.equalsIgnoreCase(id);
     }
 
     /**
@@ -160,14 +182,15 @@ public class GeographicArea {
     public void setMainPostalCode(PostalCode mainPostalCode) {
         this.mainPostalCode = mainPostalCode;
     }
-    
+
     /**
-     * Search for location listings by location with same postal code and 
-     * return distance.
-     * @param pc Posta Code to searh 
-     * @return the distance of the found Postal Code otherwise returns -1 
+     * Search for location listings by location with same postal code and return
+     * distance.
+     *
+     * @param pc Posta Code to searh
+     * @return the distance of the found Postal Code otherwise returns -1
      */
-    public double getDistanceToPostalCode(PostalCode pc){
+    public double getDistanceToPostalCode(PostalCode pc) {
         for (Location location : LocationList) {
             if (location.getPostalCode().equals(pc)) {
                 return location.getDistance();
