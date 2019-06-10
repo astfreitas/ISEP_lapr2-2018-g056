164930ca84c70e982263bbd967e9d6da1f988b73
Antonio Freitas <1141729@isep.ipp.pt>
2019-06-10 17:36:30 +0100

    Added FXML and UI class for SP Menu. [UC12] [implementaion] and related to trello boards [UC12 - Start Implement] [UC12 - JavaFX UI]

---
+++ src/main/java/lapr/project/gpsd/ui/MainMenuSPUI.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.gpsd.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import lapr.project.utils.UIUtils;
+
+/**
+ * FXML Controller class
+ *
+ * @author joaoferreira
+ */
+public class MainMenuSPUI implements Initializable {
+    
+    private Main mainApp;
+    private MainMenuUI mainMenuUI;
+    @FXML
+    private MenuItem rateSPBtn;
+    @FXML
+    private MenuItem aboutBtn;
+    @FXML
+    private MenuItem addDailyAvaiBtn;
+    @FXML
+    private MenuItem consultServiceOrders;
+    @FXML
+    private MenuItem completeServiceBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void setMainApp(Main mainApp){
+        this.mainApp = mainApp;
+    }
+
+    public void setMainMenuUI(MainMenuUI mainMenuUI) {
+        this.mainMenuUI = mainMenuUI;
+    }
+
+    public void backToMainMenu(){
+        this.mainMenuUI.toMainMenuClient();
+    }
+
+    @FXML
+    private void handleAboutBtn(ActionEvent event) {
+        UIUtils.about();
+    }
+
+    @FXML
+    private void handleAddDailyAvaiBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleConsultServiceOrdersBtn(ActionEvent event) {
+        ConsultServiceOrderUI consultServiceOrderUI = new ConsultServiceOrderUI(this, mainApp);
+    }
+
+    @FXML
+    private void handleCompleteServiceBtn(ActionEvent event) {
+    }
+}
