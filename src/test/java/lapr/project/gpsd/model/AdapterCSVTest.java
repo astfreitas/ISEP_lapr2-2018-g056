5cd95524e990016c123082c6b4a87d9c7fe4e636
João Ferreira <1181436@isep.ipp.pt>
2019-06-15 17:01:49 +0100

    RegisterClientController Test [Tests]

--- src/test/java/lapr/project/gpsd/model/AdapterCSVTest.java
+++ src/test/java/lapr/project/gpsd/model/AdapterCSVTest.java
@@ -1,5 +1,6 @@
 package lapr.project.gpsd.model;
 
+import java.io.File;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -26,6 +27,7 @@ public class AdapterCSVTest {
     @Test
     public void testExport() {
         System.out.println("export");
+        new File("./src/main/resources/testFiles/").mkdir();
         String filename = "./src/main/resources/testFiles/testExportCSV.csv";
         String[] line = {"teste1", "teste2", "teste3"};
         AdapterCSV instance = new AdapterCSV();
